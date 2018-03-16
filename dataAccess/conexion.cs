55515fcde18e83f733711bde7bf66bed941c7f69
Rafael Arguello <smukideejeah@outlook.com>
2018-03-15 22:56:06 -0600

    Creacion de conexion a base de datos

---
+++ dataAccess/conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace dataAccess
+{
+    public static class conexion
+    {
+        public static MySqlConnection mysqlcon;
+        public static int iniciar()
+        {
+            try
+            {
+                mysqlcon = new MySqlConnection("Server=35.231.83.244;Database=armoniaprueba;Uid=armonia;Pwd=am12345678;");
+                mysqlcon.Open();
+                return 1;
+            }catch(MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+    }
+}
